fc4018aa38b8ca370e1fb2aa4b17fbabe1bffde7
ngochan0420 <ptnhan915@gmail.com>
2026-04-27 00:24:41 +0700

    Update Pipe.java

--- Pipe.java
+++ Pipe.java
@@ -6,22 +6,25 @@ public class Pipe {
     int width = 64;
     int speed = 4;
 
-    int gap = 150; 
+    int gap = 150;
     int topHeight;
 
     Image topImg;
     Image bottomImg;
 
+    int boardWidth;
     int boardHeight;
+
+    boolean passed = false; 
 
     Random rand = new Random();
 
     public Pipe(int startX, int boardHeight, Image topImg, Image bottomImg) {
         this.x = startX;
         this.boardHeight = boardHeight;
+        this.boardWidth = startX;
         this.topImg = topImg;
         this.bottomImg = bottomImg;
-
         randomHeight();
     }
 
@@ -37,7 +40,6 @@ public class Pipe {
 
     public void draw(Graphics g) {
         g.drawImage(topImg, x, 0, width, topHeight, null);
-
         int bottomY = topHeight + gap;
         int bottomHeight = boardHeight - bottomY;
         g.drawImage(bottomImg, x, bottomY, width, bottomHeight, null);
@@ -45,5 +47,27 @@ public class Pipe {
 
     public boolean isOffScreen() {
         return x + width < 0;
+    }
+
+    public void reset() {
+        x = boardWidth;
+        passed = false;
+        randomHeight();
+    }
+
+    public boolean collidesWith(Player player) {
+        Rectangle playerBounds = player.getBounds();
+        Rectangle topPipe = new Rectangle(x, 0, width, topHeight);
+        int bottomY = topHeight + gap;
+        Rectangle bottomPipe = new Rectangle(x, bottomY, width, boardHeight - bottomY);
+        return playerBounds.intersects(topPipe) || playerBounds.intersects(bottomPipe);
+    }
+
+    public boolean checkAndMarkPassed(Player player) {
+        if (!passed && player.x > x + width) {
+            passed = true;
+            return true;
+        }
+        return false;
     }
 }
